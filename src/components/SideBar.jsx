bda0075dd8c258673d4ed6edabbcc8e23f1f9219
Simon Bertram <simonbertram@hotmail.com>
2024-02-10 19:12:22 +0000

    Add React icons and apply base styling to them

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,21 +1,31 @@
+import { BsPlus, BsFillLightningFill, BsGearFill } from "react-icons/bs";
+import { FaFire, FaPoo } from "react-icons/fa";
+
 const Sidebar = () => {
   return (
-    <div className="sidebar">
+    <div className="fixed top-0 left-0 h-screen w-16 bg-gray-900 text-white shadow-lg">
       <nav>
         <ul>
           <li>
-            <a href="#">Link 1</a>
+            <SideBarIcon icon={<FaFire size="28" />} />
           </li>
           <li>
-            <a href="#">Link 2</a>
+            <SideBarIcon icon={<BsPlus size="32" />} />
           </li>
           <li>
-            <a href="#">Link 3</a>
+            <SideBarIcon icon={<BsFillLightningFill size="28" />} />
+          </li>
+          <li>
+            <SideBarIcon icon={<FaPoo size="24" />} />
           </li>
         </ul>
       </nav>
     </div>
   );
+};
+
+const SideBarIcon = ({ icon }) => {
+  return <div className="sidebar-icon">{icon}</div>;
 };
 
 export default Sidebar;
